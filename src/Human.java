5f0fb720b4a8f22c7c05ccd53239265b89024aa5
Jaron <Jaronbos999@gmail.com>
2020-05-31 22:57:08 -0700

    added Human

---
+++ src/Human.java
@@ -0,0 +1,18 @@
+public class Human {
+
+    public int x,y;
+
+    Human(PApplet sketch){
+        this.x = 500/2;
+        this.y = 500/2;
+        walkerSketch = sketch;
+    }
+
+    public void display(){
+        walkerSketch.circle(this.x,this.y,5);
+    }
+    public void human(){
+        int choice = (int)random(4);
+        if (choice = )
+    }
+}
---
+++ src/Sketch.java
@@ -0,0 +1,6 @@
+import processing.core.PApplet;
+public class Sketch extend PApplet;{
+
+        public void setting() { size{500,500;}}
+        public void setup()
+}
